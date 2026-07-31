cc74bed707d7f2f51da7023ed75fc72f3abcdfaf
karthikshetty63 <200613karthik@gmail.com>
2026-07-31 23:20:57 +0530

    javascript 3

--- HTML/app.js
+++ HTML/app.js
@@ -1,92 +1,154 @@
-console.log("Hello, World!");
-let a = 5;
-let b = 8;
-let c = `the sum is:${a + b} happend`
-console.log(c);
+// console.log("Hello, World!");
+// let a = 5;
+// let b = 8;
+// let c = `the sum is:${a + b} happend`
+// console.log(c);
 
-let n = 10;
-let sum = `the sum is:${n + (n + 1)}`;
-console.log(sum);
-let x = 20;
-let y = ++x;
-let z = x++;
-console.log(y);
-console.log(z);
-let age = 23;
-let vote = (age >= 18)
-console.log(vote);
-let agev = 17
-if (agev >= 18) {
-    console.log("You are eligible to vote");
+// let n = 10;
+// let sum = `the sum is:${n + (n + 1)}`;
+// console.log(sum);
+// let x = 20;
+// let y = ++x;
+// let z = x++;
+// console.log(y);
+// console.log(z);
+// let age = 23;
+// let vote = (age >= 18)
+// console.log(vote);
+// let agev = 17
+// if (agev >= 18) {
+//     console.log("You are eligible to vote");
 
-}
-if (agev == 17) {
-    console.log("lets think");
-}
-let color = "yellow";
+// }
+// if (agev == 17) {
+//     console.log("lets think");
+// }
+// let color = "yellow";
 
-if (color === "red") {
-    console.log("stop");
+// if (color === "red") {
+//     console.log("stop");
 
-}
-else if (color === "green") {
-    console.log("go");
+// }
+// else if (color === "green") {
+//     console.log("go");
 
-}
-else if (color === "yellow") {
-    console.log("wait");
-}
-else {
-    console.log("invalid color");
-}
-let price = 150;
-if (price === 250) {
-    console.log("size XL");
+// }
+// else if (color === "yellow") {
+//     console.log("wait");
+// }
+// else {
+//     console.log("invalid color");
+// }
+// let price = 150;
+// if (price === 250) {
+//     console.log("size XL");
 
-}
-else if (price < 250 && price > 100) {
-    console.log("size L");
-}
-else if (price <= 100 && price > 50) {
-    console.log("size M");
+// }
+// else if (price < 250 && price > 100) {
+//     console.log("size L");
+// }
+// else if (price <= 100 && price > 50) {
+//     console.log("size M");
 
-}
-else if (price === 50) {
-    console.log("size S");
-}
-else {
-    console.log("invalid size");
-}
+// }
+// else if (price === 50) {
+//     console.log("size S");
+// }
+// else {
+//     console.log("invalid size");
+// }
 
-let day = "monday";
-if (day === "monday") {
-    console.log("working day");
-    {
-        if (day === "tuesday") {
-            console.log("lets party");
+// let day = "monday";
+// if (day === "monday") {
+//     console.log("working day");
+//     {
+//         if (day === "tuesday") {
+//             console.log("lets party");
 
-        }
-        else if (day === "wednesday") {
-            console.log("lets read");
-        }
-        else if (day === "thursday") {
-            console.log("lets code");
-        }
-    }
-}
-else {
-    console.log("let chill");
-}
+//         }
+//         else if (day === "wednesday") {
+//             console.log("lets read");
+//         }
+//         else if (day === "thursday") {
+//             console.log("lets code");
+//         }
+//     }
+// }
+// else {
+//     console.log("let chill");
+// }
 
-if (day === "monday" || day === "tuesday" || day === "wednesday" || day === "thursday" || day === "friday") {
-    console.log("oh god its working day");
-}
+// if (day === "monday" || day === "tuesday" || day === "wednesday" || day === "thursday" || day === "friday") {
+//     console.log("oh god its working day");
+// }
 
-let name = "good string"
-if (name[0] === "a" && name.length > 3) {
-    console.log(" good ")
+// let name = "good string"
+// if (name[0] === "a" && name.length > 3) {
+//     console.log(" good ")
 
-}
-else {
-    console.log("bad")
-}
+// }
+// else {
+//     console.log("bad")
+// }
+// let num = 12;
+// if ((num % 3 === 0) && ((num + 1 == 15) || (num - 1 == 11))) {
+//     console.log("good number");
+// }
+// else {
+//     console.log("bad number");
+// }
+// if ("") {
+//     console.log("true");
+
+// }
+// else {
+//     console.log("false");
+// }
+// let traffic = "red";
+// switch (traffic) {
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "green":
+//         console.log("go");
+//         break;
+//     case "yellow":
+//         console.log("wait");
+//         break;
+//     default:
+//         console.log("invalid color");
+// }
+// let days = "6"
+// switch (days) {
+//     case "1":
+//         console.log("monday");
+//         break;
+//     case "2":
+//         console.log("tuesday");
+//         break;
+//     case "3":
+//         console.log("wednesday");
+//         break;
+//     case "4":
+//         console.log("thursday");
+//         break;
+//     default:
+//         console.log("rest day");
+// }
+// // alert("hey somthing is wrong!🤷‍♂️");
+// // console.error("hey somthing is wrong!🤷‍♂️");
+// // let k = prompt("enter your name");
+// // console.log(k);
+// // let m = confirm("are you sure?");
+// // console.log(m);
+// let s = prompt("enter your name");
+// let p = prompt("are you sure last name?");
+// let h = `the name is :${s + " " + p}`;
+// console.log(h);
+// let strr = "good"
+// if (strr.length / 10) {
+//     console.log("good ");
+// }
+// else {
+//     console.log("bad");
+// }
---
+++ HTML/app1.js
@@ -0,0 +1,19 @@
+// let name = prompt("enter your name");
+// let age = prompt("enter your age");
+
+// if (age >= 18) {
+//     alert("you are eligible for vote");
+// }
+// else {
+//     alert("you are not eligible for vote");
+// }
+// let name1 = "           karthik         ";
+// console.log(name1);
+// name1.trim().trimEnd().trimStart();
+// console.log(name1);
+// let password = prompt("enter your pss");
+// console.log(password.trim());
+let k = "apna college";
+console.log(k.toUpperCase());
+console.log(k.toLowerCase());
+console.log(k[7]);
